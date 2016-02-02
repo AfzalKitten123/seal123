8de9630604209d9cac42cb244eef71b3746f1d01
Afzal Shama Soudagar <Afzal Shama Soudagar>
2016-02-02 21:36:58 +0530

    https://github.com/seal11/hackathon.git

---
+++ App1/Models/EventModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace App1.Models
+{
+    public class EventModel
+    {
+        [JsonProperty(PropertyName = "location")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "time")]
+        public string Time { get; set; }
+
+        [JsonProperty(PropertyName = "game")]
+        public string Game { get; set; }
+
+        public bool Hogaya{ get; set; }
+    }
+}
